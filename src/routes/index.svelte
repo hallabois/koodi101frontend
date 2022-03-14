ac0378d61a3947512a83cda9d02c0c6d1ff1ffb8
Elias Eskelinen <elias.eskelinen@pm.me>
2022-03-14 16:06:15 +0200

    dump everything

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    let dataPromise;
+    onMount(async () => {
+        fetchAccelData();
+    } );
+    async function fetchAccelData() {
+        dataPromise = await fetch("http://95.216.143.30:9000/api/acceleration");
+        console.log(dataPromise);
+        return await dataPromise.json();
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#await fetchAccelData()}
+    <p>Ladataan dataa...</p>
+{:then data} 
+    {JSON.stringify(data)}
+{/await}
